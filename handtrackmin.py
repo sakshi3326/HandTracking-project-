f5cfd3a68e8e7d9bf242a58f5dfbbb6bb36f3933
sakshi3326 <saqkshiiitian123@gmail.com>
2022-06-25 16:24:39 +0530

    identify landmark of particular id

--- handtrackmin.py
+++ handtrackmin.py
@@ -23,6 +23,8 @@ while True:
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 print(id,cx,cy)
+                if id==4:
+                    cv2.circle(img, (cx,cy), 12, (255,0,255),cv2.FILLED)
             mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
     cTime = time.time()
     fps = 1/(cTime-pTime)
