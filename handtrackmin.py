7638acf59ca20cbdf0a93b2ca38b1b361bf6e772
sakshi3326 <saqkshiiitian123@gmail.com>
2022-06-25 16:15:02 +0530

    id and landmark

--- handtrackmin.py
+++ handtrackmin.py
@@ -18,12 +18,17 @@ while True:
     #print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id,cx,cy)
             mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
 
     cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
-    
+
     cv2.imshow("image", img)
     cv2.waitKey(1)
