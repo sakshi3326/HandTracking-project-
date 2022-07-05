3934df558927cac5c6498b4635744537bcf4a53b
sakshi3326 <saqkshiiitian123@gmail.com>
2022-07-05 23:55:14 +0530

    line between the tips

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -21,9 +21,12 @@ while True:
 
         x1, y1 = lmList[4][1], lmList[4][2]
         x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1+x2)//2, (y1+y2)//2
 
-        cv2.circle(img,(x1,y1), 15, (255,0,255), cv2.FILLED)
-        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img,(x1,y1), 10, (255,0,255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+        cv2.line(img,(x1,y1),(x2,y2),(255,0,255),3)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
