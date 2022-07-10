45673f331553af2adc0748e4ccc8bc0016c1d338
sakshi3326 <saqkshiiitian123@gmail.com>
2022-07-11 00:43:45 +0530

    set the volume bar

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -2,7 +2,11 @@ import cv2
 import time
 import mediapipe
 import numpy as np
+import math
 import handtrackingmodule as htm
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 wCam, hCam = 600, 480
 
 cap = cv2.VideoCapture(0)
@@ -12,12 +16,21 @@ pTime = 0
 
 detector = htm.handDetector(detectionCon=0.7)
 
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL,None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+#volume.getMute()
+#volume.GetMAsterVolumeLevel()
+print(volume.GetVolumeRange())
+volume.SetMasterVolumeLevel(-20.0, None)
+
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
     if len(lmList)!=0:
-        print(lmList[4], lmList[8])
+        #print(lmList[4], lmList[8])
 
         x1, y1 = lmList[4][1], lmList[4][2]
         x2, y2 = lmList[8][1], lmList[8][2]
@@ -27,6 +40,13 @@ while True:
         cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
         cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
         cv2.line(img,(x1,y1),(x2,y2),(255,0,255),3)
+
+        length = math.hypot(x2-x1,y2-y1)
+        #print(length)
+
+        if length<50:
+            cv2.circle(img, (cx, cy), 10, (0,255,0), cv2.FILLED)
+
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
